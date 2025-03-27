1ef947e4bc3ff926930e13936a09120c4bf4cdb2
ivancekikj <ivan.cekic10@gmail.com>
2025-03-27 21:06:24 +0100

    Initial commit

---
+++ src/main/java/mk/com/testcraftai/testcraftai/TestCraftAiApplication.java
@@ -0,0 +1,13 @@
+package mk.com.testcraftai.testcraftai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCraftAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCraftAiApplication.class, args);
+    }
+
+}
